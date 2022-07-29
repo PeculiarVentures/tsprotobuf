356e329b23f2b96137da066e22818ced8a814073
microshine <microshine@mail.ru>
2022-07-29 23:51:59 +0300

    style(lint): format document

--- src/converter.ts
+++ src/converter.ts
@@ -1,24 +1,24 @@
-import {Convert} from "pvtsutils";
+import { Convert } from "pvtsutils";
 
 export interface IConverter<In> {
-    set: (value: In) => PromiseLike<Uint8Array>;
-    get: (value: Uint8Array) => PromiseLike<In>;
+  set: (value: In) => Promise<Uint8Array>;
+  get: (value: Uint8Array) => Promise<In>;
 }
 
 export class ArrayBufferConverter {
-    public static async set(value: ArrayBuffer) {
-        return new Uint8Array(value);
-    }
-    public static async get(value: Uint8Array) {
-        return new Uint8Array(value).buffer;
-    }
+  public static async set(value: ArrayBuffer): Promise<Uint8Array> {
+    return new Uint8Array(value);
+  }
+  public static async get(value: Uint8Array): Promise<ArrayBufferLike> {
+    return new Uint8Array(value).buffer;
+  }
 }
 
 export class StringConverter {
-    public static async set(value: string) {
-        return new Uint8Array(Convert.FromUtf8String(value));
-    }
-    public static async get(value: Uint8Array) {
-        return Convert.ToUtf8String(value);
-    }
+  public static async set(value: string): Promise<Uint8Array> {
+    return new Uint8Array(Convert.FromUtf8String(value));
+  }
+  public static async get(value: Uint8Array): Promise<string> {
+    return Convert.ToUtf8String(value);
+  }
 }
